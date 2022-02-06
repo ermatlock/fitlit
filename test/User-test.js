f9fbf92153c6f4b303ab21d51500e1cccb4576dc
Emili Kaiman <emilikaiman@gmail.com>
2022-02-06 13:36:43 -0700

    Add user data

---
+++ test/User-test.js
@@ -0,0 +1,42 @@
+import { expect } from "chai";
+import User from "../src/User";
+
+describe("User", () => {
+  let userData1;
+  let userData2;
+  let user1;
+  let user2;
+
+  beforeEach(() => {
+    userData1 = {
+      id: 1,
+      name: "Luisa Hane",
+      address: "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      email: "Diana.Hayes1@hotmail.com",
+      strideLength: 4.3,
+      dailyStepGoal: 10000,
+      friends: [16, 4, 8],
+    };
+
+    userData2 = {
+      id: 2,
+      name: "Jarvis Considine",
+      address: "30086 Kathryn Port, Ciceroland NE 07273",
+      email: "Dimitri.Bechtelar11@gmail.com",
+      strideLength: 4.5,
+      dailyStepGoal: 5000,
+      friends: [9, 18, 24, 19],
+    };
+
+    user1 = new User(userData1);
+    user2 = new User(userData2)
+  });
+
+  it("should be a function", function () {
+    expect(User).to.be.a("function");
+  });
+
+  it("should be an instance of userRepository", () => {
+    expect(user1).to.be.an.instanceOf(User);
+  });
+});
