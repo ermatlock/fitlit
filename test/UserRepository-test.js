4e3946ad9cbb5fb3093a2039ebfd1eba11d096df
Emili Kaiman <emilikaiman@gmail.com>
2022-02-06 12:06:13 -0700

    Create userRepository class

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -2,6 +2,11 @@ import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
 
 describe('User Repository', () => {
+
+  beforeEach( () => {
+    userRepository = new UserRepository()
+  })
+
   it('should be a function', function () {
     expect(UserRepository).to.be.a('function');
   });
