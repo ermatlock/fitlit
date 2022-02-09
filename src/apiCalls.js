6785f0503956039050a48153c1960c0ac9383620
Emily Corcoran <cathleencorcoran@gmail.com>
2022-02-09 12:44:51 -0500

    Create api calls for all categories

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,34 @@
-// Your fetch requests will live here!
+//FETCH calls
+
+export const fetchUserData = () => {
+  return fetch("https://fitlit-api.herokuapp.com/api/v1/users")
+  .then((response) => response.json())
+  .then(data => console.log(data))
+  .catch((error) => console.log(error));
+};
+
+export const fetchSleepData = () => {
+  return fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
+  .then((response) => response.json())
+  .then(data => console.log(data))
+  .catch((error) => console.log(error));
+};
+
+export const fetchActivityData = () => {
+  return fetch("https://fitlit-api.herokuapp.com/api/v1/activity")
+  .then((response) => response.json())
+  .then(data => console.log(data))
+  .catch((error) => console.log(error));
+};
+
+export const fetchHydrationData = () => {
+  return fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
+  .then((response) => response.json())
+  .then(data => console.log(data))
+  .catch((error) => console.log(error));
+};
 
 
-console.log('I will be a fetch request!')
 
+
+//POST request
