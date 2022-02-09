41c6fd25e6ea17e0eb6d1cede1459bbf6c11804e
Eric Matlock <ermatlock@gmail.com>
2022-02-08 20:48:22 -0500

    Create createUser function

--- src/scripts.js
+++ src/scripts.js
@@ -4,9 +4,23 @@ import userData from "./data/users";
 import UserRepository from "./UserRepository";
 import User from "./User";
 
+const currentUserRepository = new UserRepository();
+let currentUser;
+
 const userName = document.getElementById("userName");
 const userAddress = document.getElementById("userAddress");
 const userEmail = document.getElementById("userEmail");
 const userStrideLength = document.getElementById("userStrideLength");
 const userDailyStep = document.getElementById("userDailyStep");
 const userFriends = document.getElementById("userFriends");
+
+const createUser = (id) => {
+  currentUser = currentUserRepository.createUser(id);
+};
+
+const updateUserCard = (id) => {
+  createUser(5);
+  userName.innerText = currentUser.getFirstName();
+  userAddress.innerText = currentUser.address;
+  user;
+};
