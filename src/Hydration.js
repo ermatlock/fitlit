d869b10befe9215040d7e331fa049e2bcd2ded35
Emili Kaiman <emilikaiman@gmail.com>
2022-02-09 19:20:19 -0700

    Create ability for class to hold API data

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,7 +1,12 @@
-class Hydration {
-  constructor () {
+import User from "./User";
 
+class Hydration {
+  constructor (user, apiData) {
+    this.userId = user.id
+    this.hydrationData  = apiData.filter(person => person.userID === this.userId )
   }
+
+
 }
 
 export default Hydration
--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -1,14 +1,31 @@
 import { expect } from "chai";
-import Hydration from "../src/Hydration";
 import UserRepository from "../src/UserRepository";
 import User from "../src/User";
+import Hydration from "../src/Hydration";
 
 
 describe('Hydration', () => {
 let hydration1;
+let user1;
+let userData;
+let hydrationData
 
-  beforeEach( () => {
-    hydration1 = new Hydration()
+  beforeEach(() => {
+    hydrationData = [
+      {"userID":1,"date":"2019/06/16","numOunces":69},{"userID":2,"date":"2019/06/16","numOunces":91},{"userID":3,"date":"2019/06/16","numOunces":99},{"userID":1,"date":"2019/06/17","numOunces":96},{"userID":2,"date":"2019/06/17","numOunces":96},{"userID":3,"date":"2019/06/17","numOunces":28},{"userID":1,"date":"2019/06/18","numOunces":61},{"userID":2,"date":"2019/06/18","numOunces":70},{"userID":3,"date":"2019/06/18","numOunces":40}
+    ]
+
+    userData = {
+      id: 1,
+      name: "Luisa Hane",
+      address: "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      email: "Diana.Hayes1@hotmail.com",
+      strideLength: 4.3,
+      dailyStepGoal: 10000,
+      friends: [16, 4, 8],
+    }
+    user1 = new User(userData)
+    hydration1 = new Hydration(user1, hydrationData)
   })
 
   it('should be a function', () => {
@@ -19,5 +36,17 @@ let hydration1;
     expect(hydration1).to.be.an.instanceOf(Hydration)
   })
 
-  it('should ')
+  it('should accept a User and take the id', () => {
+    expect(hydration1.userId).to.equal(1)
+    expect(hydration1.userId).to.eql(user1.id)
+  })
+
+  it('should accept Hydration API data', () => {
+    let userOneData = hydrationData.filter(user => user.userID === 1)
+    expect(hydration1.hydrationData).to.eql(userOneData)
+  })
+
+  it('should calcu', () => {
+
+  })
 })
