86ed83ce61225e9372b77343ed66ca01a72d886e
Emili Kaiman <emilikaiman@gmail.com>
2022-02-09 19:42:46 -0700

    Create funtion to find OZ by day

--- src/Hydration.js
+++ src/Hydration.js
@@ -6,6 +6,18 @@ class Hydration {
     this.hydrationData  = apiData.filter(person => person.userID === this.userId )
   }
 
+  getAllWaterAvg() {
+    let totalOz = this.hydrationData.reduce((total, currDay) => {
+      total += currDay.numOunces
+      return total
+    }, 0)
+    let average = totalOz/this.hydrationData.length
+    return Math.round(average)
+  }
+
+  findOzByDate(date) {
+    return this.hydrationData.find(day => day.date === date).numOunces
+  }
 
 }
 
--- test/Hydration-test.js
+++ test/Hydration-test.js
@@ -12,7 +12,7 @@ let hydrationData
 
   beforeEach(() => {
     hydrationData = [
-      {"userID":1,"date":"2019/06/16","numOunces":69},{"userID":2,"date":"2019/06/16","numOunces":91},{"userID":3,"date":"2019/06/16","numOunces":99},{"userID":1,"date":"2019/06/17","numOunces":96},{"userID":2,"date":"2019/06/17","numOunces":96},{"userID":3,"date":"2019/06/17","numOunces":28},{"userID":1,"date":"2019/06/18","numOunces":61},{"userID":2,"date":"2019/06/18","numOunces":70},{"userID":3,"date":"2019/06/18","numOunces":40}
+      {"userID":1,"date":"2019/06/16","numOunces":69},{"userID":2,"date":"2019/06/16","numOunces":91},{"userID":3,"date":"2019/06/16","numOunces":99},{"userID":1,"date":"2019/06/17","numOunces":96},{"userID":2,"date":"2019/06/17","numOunces":96},{"userID":3,"date":"2019/06/17","numOunces":28},{"userID":1,"date":"2019/06/18","numOunces":61},{"userID":2,"date":"2019/06/18","numOunces":70},{"userID":3,"date":"2019/06/18","numOunces":40},{"userID":1,"date":"2019/06/19","numOunces":91},{"userID":2,"date":"2019/06/19","numOunces":76},{"userID":3,"date":"2019/06/19","numOunces":85},{"userID":1,"date":"2019/06/20","numOunces":50},{"userID":2,"date":"2019/06/20","numOunces":71},{"userID":3,"date":"2019/06/20","numOunces":51},{"userID":1,"date":"2019/06/21","numOunces":50},{"userID":2,"date":"2019/06/21","numOunces":27},{"userID":3,"date":"2019/06/21","numOunces":41},{"userID":1,"date":"2019/06/22","numOunces":43},{"userID":2,"date":"2019/06/22","numOunces":58},{"userID":3,"date":"2019/06/22","numOunces":78},
     ]
 
     userData = {
@@ -46,7 +46,15 @@ let hydrationData
     expect(hydration1.hydrationData).to.eql(userOneData)
   })
 
-  it('should calcu', () => {
+  it('should calculate one Users average fluid oz of all time', () => {
+    expect(hydration1.getAllWaterAvg()).to.equal(75)
+  })
 
+  it('should find a date and return how many Oz drank that day', () => {
+    expect(hydration1.findOzByDate("2019/06/16")).to.equal(69)
+  })
+
+  it('should show each days water consumtion for a week', () => {
+    expect(hydration1.getWeeksWater())
   })
 })
