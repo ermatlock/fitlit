4f63e4a88c946781bf372c98a935310036a2fce3
Eric Matlock <ermatlock@gmail.com>
2022-02-12 18:04:34 -0500

    Create userId and activityData properties

--- src/Activity.js
+++ src/Activity.js
@@ -1,5 +1,10 @@
 class Activity {
-  constructor() {}
+  constructor(id, activityData) {
+    this.userId = id;
+    this.activityData = activityData.filter(
+      (person) => person.userID === this.userId
+    );
+  }
 }
 
 export default Activity;
