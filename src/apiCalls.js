09b920d9158491f4cb682f9a340029bbe4ea29fb
Emily Corcoran <cathleencorcoran@gmail.com>
2022-02-25 18:05:47 -0500

    change fetch call URL to new server

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,27 +1,27 @@
 // //FETCH calls
 //
 const fetchUserData = () => {
-  return fetch("https://fitlit-api.herokuapp.com/api/v1/users")
+  return fetch("http://localhost:3001/api/v1/users")
   .then((response) => response.json())
   .catch(error => console.log(error))
 }
 
 
 const fetchSleepData = () => {
-  return fetch("https://fitlit-api.herokuapp.com/api/v1/sleep")
+  return fetch("http://localhost:3001/api/v1/sleep")
     .then((response) => response.json())
     .catch(error => console.log(error))
 }
 
 const fetchActivityData = () => {
-  return fetch("https://fitlit-api.herokuapp.com/api/v1/activity")
+  return fetch("http://localhost:3001/api/v1/activity	")
     .then((response) => response.json())
     .catch(error => console.log(error))
 
 }
 
 const fetchHydrationData = () => {
-  return fetch("https://fitlit-api.herokuapp.com/api/v1/hydration")
+  return fetch("http://localhost:3001/api/v1/hydration")
     .then((response) => response.json())
     .catch(error => console.log(error))
 };
