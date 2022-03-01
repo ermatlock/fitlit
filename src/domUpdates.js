04dea6e9e669ceaff2991be599c0bb08560234d8
Eric Matlock <ermatlock@gmail.com>
2022-02-28 23:50:10 -0500

    Re-add error window function listeners and selectors

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -7,7 +7,28 @@ import {
   updateSleepQualityChart,
 } from "./ourCharts";
 
+/*~~~~~~~~~~~~~~~QUERY SELECTORS~~~~~~~~~~~~~~~~~*/
+const modal = document.getElementById("myModal");
+const span = document.getElementsByClassName("close")[0];
+const errorMessage = document.getElementById("errorMessage");
+
+/*~~~~~~~~~~~~~~~EVENT LISTENERS~~~~~~~~~~~~~~~~~*/
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+/*~~~~~~~~~~DOM MANIPULATION FUNCTIONS~~~~~~~~~~~*/
 let domUpdates = {
+  showError(message) {
+    errorMessage.innerText = message;
+    modal.style.display = "block";
+  },
+
   greetUser(currentUser) {
     welcome.innerText = `Welcome, ${currentUser.getFirstName()}`;
   },
